4fe6438deb228c1cc76920521b74a96c1bcc5fd7
Iynix2023 <xinyi.shen.2023@smu.edu.sg>
2025-03-13 16:34:15 +0800

    Testing Own Branch Adding .env
    testing adding .env

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,12 @@ import { ClerkProvider } from '@clerk/clerk-react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { AppRoutes } from '@/routes';
 
-const CLERK_PUBLISHABLE_KEY = 'your_clerk_publishable_key';
+const CLERK_PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+
+if (!CLERK_PUBLISHABLE_KEY) {
+  throw new Error("Missing Clerk Publishable Key! Check your .env file.");
+}
 
 function App() {
   return (
